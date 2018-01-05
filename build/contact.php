6b6013f5127c4b01b84d919545856f2e549fcbb6
Almaz <almaz9527@gmail.com>
2018-01-05 23:10:43 +0500

    updated

--- build/contact.php
+++ build/contact.php
@@ -1,40 +1,23 @@
 <?php
 
-// Blank message to start with so we can append to it.
-$message = '';
+include('bots/connect.php');
 
-// Check that name & email aren't empty.
-if( empty($_POST['formcontactmethod']) ){
-    die('notfilled');
+$contact = $_REQUEST['formcontactmethod'];
+$text = $_REQUEST['formmessage'];
+
+if($text && $contact) {
+    $tgText = "Контакт: $contact\n" .
+        "Сообщение: " . $text;
+
+    foreach($tgids as $tgid) {
+        $lazy->sendMessage([
+            'chat_id' => $tgid,
+            'text' => $tgText,
+            'parse_mode' => 'Markdown'
+        ]);
+    }
+    echo 1;
 }
-
-$formcontactmethod = $_POST['formcontactmethod'];
-$formmessage = $_POST['formmessage'];
-
-$message .= "<html><body>";
-$message .= "<b>Способ связи:</b> " . $formcontactmethod . "<br />";
-$message .= "<b>Сообщение:</b> " . $formmessage;
-$message .= "</body></html>";
-
-$to = 'mail@indimedia.ru';
-
-$subject = 'Заявка с формы на сайте';
-
-$from = 'Форма на сайте';
-
-$fromEmail = 'contactform@indimedia.ru';
-
-$header = 'MIME-Version: 1.0' . "\r\n";  
-$header .= 'Content-type: text/html; charset=utf-8' . "\r\n";  
-
-$header .= "From:" . $fromEmail . "\r\n" .  
-       "Reply-To: order \r\n" .  
-       "X-Mailer: PHP/" . phpversion(); 
-
-
-// Try sending the email
-if( !mail($to, $subject, $message, $header) ){
-    die('fail');
-} else {
-    die('success');
+else {
+    echo 0;
 }
